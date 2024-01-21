f5f8c13bd64113a8882af091a0d4d4e8f44ae425
shree-2802 <pattupadmashree@gmail.com>
2024-01-21 09:18:23 +0530

    jsx

---
+++ src/Components/index.js
@@ -0,0 +1,17 @@
+import Header from './header/header';
+import Footer from './footer/footer';
+import Home from './home/home';
+import MovieCard from './movie-card/movieCard';
+import MovieDetailing from './movie-detailing/movieDetailing';
+import Movielisting from './movie-listing/movielisting';
+import PageNotFound from './page-not-found/pageNotFound';
+
+export {
+  Header,
+  Footer,
+  Home,
+  MovieCard,
+  MovieDetailing,
+  Movielisting,
+  PageNotFound,
+};
