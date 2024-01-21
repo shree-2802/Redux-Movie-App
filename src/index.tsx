18f9a9af4e49695bf78c1985aee1fb73e7cd4f66
shree-2802 <pattupadmashree@gmail.com>
2024-01-21 17:59:05 +0530

    tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import { Provider } from 'react-redux';
+import { store } from './redux/index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
